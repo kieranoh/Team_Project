cd772306c7075a1c09b1dba45cbfcb6ad398ad3a
kieranoh <77526407+kieranoh@users.noreply.github.com>
2023-05-31 13:37:07 +0900

    bullet move()

--- src/tp/GameObject.java
+++ src/tp/GameObject.java
@@ -1,112 +1,158 @@
 package tp;
 
 import java.util.*;
+import java.math.*;
 
 class GameObject {
 	int x; //현재 x좌표
 	int y; //현재 y좌표
 	boolean visible; //GUI에서 표시되는지?
-	public void run() {
-		return;
-	}
 }
 
 class Enemy extends GameObject {
-	int velocity = 1;
-	int health = 1000;
+	int velocity = 2;
+	int health;
 	int index = 0;
 	int size = 90;
-	int dx = (1000 - size)/9;
+	int dx = ((1000 - size)/9)-1;
 	int dy = (500 - size)/4;
-	int[][] road = new int[][] {
-		{4,0},{4,1},{3,1},{2,1},{1,1},{0,1},{0,2},{0,3},{1,3},
-		{2,3},{3,3},{4,3},{4,4},{4,5},{3,5},{2,5},{1,5},{0,5},
-		{0,6},{0,7},{0,8},{0,9},{1,9},{2,9},{3,9},{3,8},{3,7},
-		{4,7},{4,8},{4,9},{4,9};
 	Enemy() {
-		this.health = 1000 + Board.round*100;
+		this.health = 1000;
+		this.x = 5;
+		this.y = 405;
 		this.visible = false;
 	}
-	void move(MainTower target) {
-		if(index ==0)
-		{
-			this.x =  road[0][1] * dx ;
-			this.y = road[0][0] * dy;
+	void move() {
+		if(index == 0) {
+			this.x = Board.road[0][1] * dx;
+			this.y = Board.road[0][0] * dy;
 			index = 1;
-
 		}
-		if( (this.x )== road[this.index][1]*dx && (this.y) == road[this.index][0]*dy)
-		{
+		if(this.x == Board.road[this.index][1]*dx && this.y == Board.road[this.index][0]*dy ) {
 			this.index +=1;
 		}
-		if(index == 30)
-		{
-			this.attack(target);
-			return;
-		}
-		this.x += (road[this.index][1] - road[this.index-1][1])  * this.velocity;
-		this.y += (road[this.index][0] - road[this.index-1][0])  * this.velocity;
+		this.x += (Board.road[this.index][1] - Board.road[this.index-1][1]) * this.velocity;
+		this.y += (Board.road[this.index][0] - Board.road[this.index-1][0]) * this.velocity;
+		
 	}
-	void attack(MainTower target)
-	{
-		target.health -= this.health;
-		this.health = 0;
+	void Hit(int atk) {
+		this.health -= atk;
+		if(health <= 0) {
+			this.visible = false;
+			this.health = 0;
+		}
 	}
 }
 
 class Bullet extends GameObject {
 	int velocity;
-	int objectx;
-	int objecty;
 	double direction; //rad형태로 바뀌겠죠.
-	Bullet(int x,int y,int objectx,int objecty) {
-		this.x = x;
-		this.y = y;
-		this.objectx = objectx;
-		this.objecty = objecty;
-	}
-	void GetDirection() {
+	double dx;
+	double dy;
+	double distance=0;
+	Bullet() {
+		this.visible = false;
+		this.velocity = 10;
 		
 	}
 	void move() {
-		//Enemy와 비슷.
-		//어떤 목표를 찍으면 그 방향으로 쭉 나아가게 됨.
+		Enemy target = Board.enemylist.get(0);
+		if(this.distance == 0)
+		{
+			this.distance = Math.pow((target.y - this.y) , 2) + Math.pow((target.x - this.x) , 2);
+			this.distance = Math.sqrt(this.distance);
+			this.dy = -(this.distance * Math.sin(this.direction)/100);
+			this.dx = -(this.distance * Math.cos(this.direction)/100);
+		}
+		System.out.println("distance : "+this.distance);
+		System.out.println("direction : "+this.direction);
+		System.out.println("dx : "+this.dx+", dy : "+this.dy);
+		System.out.println("this.x : "+this.x+", this.y : "+this.y);
+		System.out.println("target.x : "+target.x+", target.y : "+target.y);
+		this.x += (int)this.dx * this.velocity;
+		this.y += (int)this.dy * this.velocity;
+	}
+	void GetDirection() {
+		
+		Enemy target = Board.enemylist.get(0);
+		
+		this.direction = Math.atan2(target.y - this.y, target.x - this.x);
+		
+		
 	}
 	boolean isHit() {
 		if(x<0 || x>1000 || y<0 || y>500)
 			return true;
-		
+		for(int i=0; i<15; i++) {
+			Enemy target = Board.enemylist.get(i);
+			if(target.visible == true) {
+				int x2 = (target.x - this.x)*(target.x - this.x);
+				int y2 = (target.y - this.y)*(target.y - this.y);
+				if(x2 + y2 <= 1600)
+				{
+					System.out.println("hit");
+					return true;
+				}
+					
+			}
+		}
 		return false;
 	}
 }
 class Tower extends GameObject {
 	int atk = 50;//체력은 얼마나 깎을건지?
-	int reload = 5; //몇 Cycle마다 한번 쏘는지?
+	int reload = 50; //몇 Cycle마다 한번 쏘는지?
 	int target = -1; //ArrayList의 몇번째 Enemy를 쏠건지?
 	double radian;
-	int cnt = 0;
+	int cnt = 1;
 	Tower(int x,int y) {
 		this.x = x;
 		this.y = y;
 		this.visible = false;
 	}
-	
-	void SetDirection() {
-		
+	//랜덤은 나중에 생각할 문제
+	//지금은 무조건 앞에 있는걸 때림
+	boolean SetTarget() {
+		this.target = -1;
+		for(int i=0; i<15; i++) {
+			if(Board.enemylist.get(i).visible == true) {
+				this.target = i;
+				return true;
+			}
+		}
+		return false;
 	}
-	
+	boolean Is_Valid() {
+		if(Board.enemylist.get(this.target).visible == true)
+			return true;
+		return false;
+	}
 	void Shoot() {
-		
-	}
-}
-
-
-class MainTower extends GameObject{
-	int health;
-	int x;
-	int y;
-	MainTower(int health)
-	{
-		this.health = health;
+		if(cnt%reload == 0) {
+			if(this.target == -1 || Is_Valid() == false) {
+				boolean hastarget = this.SetTarget();
+				if(hastarget == false)
+					return;
+			}
+			double bias = 1e-9;
+			int ox = Board.enemylist.get(this.target).x;
+			int oy = Board.enemylist.get(this.target).y;
+			double rad = Math.atan2((double)(this.y-oy)+bias,(double)(this.x-ox)+bias);
+			this.radian = rad;
+			for(int i=0; i<1000; i++) {
+				Bullet temp = Board.bulletlist.get(i);
+				if(temp.visible == true)
+					continue;
+				System.out.println("Bullet : " + i + "created");
+				temp.visible = true;
+				temp.x = this.x;
+				temp.y = this.y;
+				temp.direction = rad;
+				break;
+			}
+			cnt = 1;
+		} else {
+			this.cnt++;
+		}
 	}
 }
